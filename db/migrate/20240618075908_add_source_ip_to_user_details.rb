8c83a1ebed87110408966c1a3bbac2067eeb3e7a
Anil Kumar <aniltimt@gmail.com>
2024-09-26 14:54:40 +0530

    more fix

--- db/migrate/20240618075908_add_source_ip_to_user_details.rb
+++ db/migrate/20240618075908_add_source_ip_to_user_details.rb
@@ -1,6 +1,7 @@
 class AddSourceIpToUserDetails < ActiveRecord::Migration[7.1]
   def change
     #add_column :user_details, :source_ip, :string
-    change_column :recently_addeds, :subtitle, :text
+    #change_column :recently_addeds, :subtitle, :text
+    change_column :qr_data, :codeData, :text
   end
 end
