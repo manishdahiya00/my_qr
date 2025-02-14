eefa26cf0120541387d801f5f05d667b8046a2ed
Anil Kumar <aniltimt@gmail.com>
2025-02-14 13:53:21 +0530

    minor fix

--- app/controllers/api/v1/appOpenApi.rb
+++ app/controllers/api/v1/appOpenApi.rb
@@ -24,6 +24,9 @@ module API
 
         post do
           begin
+            social_links = [{id: 1, title: 'Telegram', actionUrl: 'https://t.me/mobcandyapp'},{id: 2, title: 'Whatsapp', actionUrl: 'https://whatsapp.com/channel/0029Vb3vPT60AgW39nxpC624'},
+              {id: 3, title: 'Facebook', actionUrl: 'https://www.facebook.com/gamersanjuyt/'},{id: 4, title: 'YouTube', actionUrl: 'https://www.youtube.com/channel/UCLV_ZFqTH9l4zx8VFZ-Xjqw'},
+              {id: 5, title: 'TwitterX', actionUrl: 'https://x.com/mobcandyapp'},{id: 6, title: 'Instagram', actionUrl: 'https://www.instagram.com/cashleyapp/'}]
             @app_open = AppOpen.find_by(securityToken: params[:securityToken])
             unless @app_open.present?
               @app_open = AppOpen.create(
@@ -32,27 +35,26 @@ module API
                 versionCode: params[:versionCode],
                 securityToken: params[:securityToken]
               )
-              { status: 200, message: "Success",socialName: @app_open.socialName, socialEmail: @app_open.socialEmail,socialImgUrl: @app_open.socialImgUrl,appUrl:@app_open.app_url,forceUpdate:@app_open.forceUpdate }
-            end
-            social_links = [{id: 1, title: 'Telegram', actionUrl: 'https://t.me/mobcandyapp'},{id: 2, title: 'Whatsapp', actionUrl: 'https://whatsapp.com/channel/0029Vb3vPT60AgW39nxpC624'},
-              {id: 3, title: 'Facebook', actionUrl: 'https://www.facebook.com/gamersanjuyt/'},{id: 4, title: 'YouTube', actionUrl: 'https://www.youtube.com/channel/UCLV_ZFqTH9l4zx8VFZ-Xjqw'},
-              {id: 5, title: 'TwitterX', actionUrl: 'https://x.com/mobcandyapp'},{id: 6, title: 'Instagram', actionUrl: 'https://www.instagram.com/cashleyapp/'}]
+              {status: 200, message: "Success",socialName: @app_open.socialName, socialEmail: @app_open.socialEmail,socialImgUrl: @app_open.socialImgUrl,
+                appUrl:@app_open.app_url,forceUpdate:@app_open.forceUpdate,socialLinks: social_links}
+            end            
             if params[:userId] == ""
-               { status: 200, message: "No User Found", socialName: "" ,socialEmail: "",socialImgUrl: "",appUrl: "",forceUpdate:@app_open.forceUpdate}
+               {status: 200, message: "No User Found", socialName: "" ,socialEmail: "",socialImgUrl: "",appUrl: "",
+                forceUpdate:@app_open.forceUpdate,socialLinks: social_links}
             else
               @user = UserDetail.find_by(securityToken:params[:securityToken])
               if @user
-              @app = AppOpen.find_by(securityToken: params[:securityToken])
-              @new_app_open = @app.update(
-                socialName: @user.socialName || "USER",
-                socialEmail: @user.socialEmail,
-                socialImgUrl:@user.socialImgUrl
-              )              
-              {status: 200, message: "Success",socialName: @app.socialName, socialEmail: @app.socialEmail,socialImgUrl: @app.socialImgUrl, socialLinks: social_links,
-                appUrl:@app.app_url || "https://play.google.com/store/apps/details?id=com.apps.scanbuddy",forceUpdate:@app.forceUpdate}
-            else
-              {status: 200, message: "No User Found", socialName: "" ,socialEmail: "",socialImgUrl: "",appUrl: "",forceUpdate:@app_open.forceUpdate,socialLinks: social_links}
-            end
+                @app = AppOpen.find_by(securityToken: params[:securityToken])
+                @new_app_open = @app.update(
+                  socialName: @user.socialName || "USER",
+                  socialEmail: @user.socialEmail,
+                  socialImgUrl:@user.socialImgUrl
+                )              
+                {status: 200, message: "Success",socialName: @app.socialName, socialEmail: @app.socialEmail,socialImgUrl: @app.socialImgUrl, socialLinks: social_links,
+                 appUrl:@app.app_url || "https://play.google.com/store/apps/details?id=com.apps.scanbuddy", forceUpdate:@app.forceUpdate}
+              else
+                {status: 200, message: "No User Found", socialName: "" ,socialEmail: "",socialImgUrl: "", appUrl: "",forceUpdate:@app_open.forceUpdate,socialLinks: social_links}
+              end
             end
           rescue => e
             { message: "Error", status: 500, error: e }
